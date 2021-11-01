533d552375f42cf4ac8d76adc5f5ae0df098fa05
wickyang <wickyang@yunify.com>
2021-11-01 10:33:51 +0800

    chore: 更新overview change tsx to jsx

--- src/views/Overview/Services/Services.tsx
+++ src/views/Overview/Services/Services.tsx
@@ -6,8 +6,8 @@ import FlowCell from './FlowCell'
 
 const Content = tw(
   CardContent
-)`py-2.5 flex font-medium text-sm text-neut-15 h-80 space-x-4`
-const Main = tw.div`border border-green-4 border-dashed rounded-sm bg-green-0 px-12 2xl:px-[68px] flex items-center flex-1`
+)`py-2.5 flex font-medium text-sm text-neut-15 space-x-4`
+const Main = tw.div`border border-green-4 border-dashed h-[300px] rounded-sm bg-green-0 px-12 2xl:px-[68px] flex items-center flex-1`
 const Sider = tw.div`border border-[#BAE6FD] border-dashed rounded-sm bg-[#ECFBFF] flex justify-center min-w-[200px] w-2/12`
 
 function Services() {
