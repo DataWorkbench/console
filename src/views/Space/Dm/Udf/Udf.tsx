dfafc61afb27b0f5aa3bb758e1cfc8c8efdc3da8
yazhouhu <yazhouhu@yunify.com>
2021-11-12 11:09:57 +0800

    feat: replace udf header icon

--- src/views/Space/Dm/Udf/Udf.tsx
+++ src/views/Space/Dm/Udf/Udf.tsx
@@ -14,7 +14,7 @@ const pageTabsData = [
     title: '函数管理',
     description:
       '用户在编辑函数代码时支持类似工程方式的管理，可以创建文件、文件夹并对其进行编辑。如果用户代码是上传zip包的方式，则前端进行相应解压展示，并支持用户在线编辑修改。',
-    icon: 'name-space',
+    icon: 'textarea',
     helpLink: '/compute/vm/',
   },
 ]
