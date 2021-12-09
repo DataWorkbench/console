db70edfe8df0a1b49fa40ff8948ebeb95974eef9
yazhouhu <yazhouhu@yunify.com>
2021-12-09 17:42:57 +0800

    feat: header add helpcenter tooltip

--- src/views/Space/Header/Settings.tsx
+++ src/views/Space/Header/Settings.tsx
@@ -1,35 +1,103 @@
 import { Icon } from '@QCFE/lego-ui'
 import { get } from 'lodash-es'
-import { Center } from 'components'
+import tw, { styled, css } from 'twin.macro'
+import { Modal, HelpCenterModal } from '@QCFE/qingcloud-portal-ui'
 
-export const Settings = ({ darkMode }) => (
-  <Center>
-    <Icon
-      name="bell"
-      size="medium"
-      type={darkMode ? 'light' : 'dark'}
-      changeable
-      tw="mr-2 cursor-pointer"
-    />
-    <Icon
-      name="cogwheel"
-      type={darkMode ? 'light' : 'dark'}
-      size="medium"
-      changeable
-      tw="mr-2 cursor-pointer"
-    />
-    <Icon
-      name="documentation"
-      type={darkMode ? 'light' : 'dark'}
-      size="medium"
-      changeable
-      tw="mr-2 cursor-pointer"
-    />
-    <span tw="mr-2 dark:text-white">{get(window, 'USER.user_name', '')}</span>
-    <span tw="mr-6 inline-block bg-neut-2 dark:bg-neut-13 dark:text-white px-2 py-0.5 rounded-2xl">
-      项目所有者
-    </span>
-  </Center>
-)
+import { Center, Tooltip } from 'components'
+
+const IconBox = styled(Center)(() => {
+  return [
+    css`
+      &:hover {
+        .icon {
+          .qicon {
+            fill: #9ddfc9;
+            color: #15a675;
+          }
+        }
+      }
+    `,
+    tw`cursor-pointer hover:dark:bg-neut-13 hover:bg-neut-1`,
+  ]
+})
+
+const IconBoxWithTootip = styled(Center)(() => {
+  return [
+    css`
+      &{
+        [aria-expanded='true'] {
+          .header-icon-bg-box {
+            ${tw`cursor-pointer dark:bg-neut-13 bg-neut-1`}
+          }
+        .icon {
+          .qicon {
+            fill: #9ddfc9;
+            color: #15a675;
+          }
+        }
+      }
+    `,
+  ]
+})
+export const Settings = ({ darkMode }) => {
+  const handleOpenHelpCenter = (link: string) => {
+    const openModal = Modal.open(HelpCenterModal, {
+      link,
+      onCancel: () => Modal.close(openModal),
+    })
+  }
+
+  return (
+    <Center>
+      <IconBoxWithTootip>
+        {/* <IconBox size={28} tw="mr-2">
+        <Icon
+          name="bell"
+          size={20}
+          type={darkMode ? 'light' : 'dark'}
+          changeable
+        />
+      </IconBox>
+      <IconBox size={28} tw="mr-2 ">
+        <Icon
+          name="cogwheel"
+          type={darkMode ? 'light' : 'dark'}
+          size={20}
+          changeable
+        />
+      </IconBox> */}
+        <Tooltip
+          theme={darkMode ? 'light' : 'dark'}
+          content={<div tw="text-white dark:text-neut-13  p-3">帮助中心</div>}
+        >
+          <IconBox
+            onClick={
+              // TODO:  设置文档 url
+              () => handleOpenHelpCenter('/')
+            }
+            className="header-icon-bg-box"
+            size={28}
+            tw=" hover:dark:bg-neut-13 hover:bg-neut-1"
+          >
+            <Icon
+              name="documentation"
+              type={darkMode ? 'light' : 'dark'}
+              changeable
+              size={20}
+              tw="cursor-pointer"
+            />
+          </IconBox>
+        </Tooltip>
+      </IconBoxWithTootip>
+
+      <span tw="ml-5 mr-2 dark:text-white font-semibold">
+        {get(window, 'USER.user_name', '')}
+      </span>
+      <span tw="mr-6 inline-block bg-neut-2 dark:bg-neut-13 dark:text-white px-2 py-0.5 rounded-2xl">
+        项目所有者
+      </span>
+    </Center>
+  )
+}
 
 export default Settings
