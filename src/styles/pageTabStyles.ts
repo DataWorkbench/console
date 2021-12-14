83c9d60ec87f94b8207d7efec20a69943ab0d08e
yazhouhu <yazhouhu@yunify.com>
2021-12-14 15:17:07 +0800

    style: move style code

--- src/styles/pageTabStyles.ts
+++ src/styles/pageTabStyles.ts
@@ -1,6 +1,9 @@
 import tw, { css } from 'twin.macro'
 
 const pageTabStyles = css`
+  .page-tab-container .collapse-panel .tab-title {
+    ${tw`font-medium`}
+  }
   .collapse-panel .panel-right .icon {
     ${tw`-top-12`}
   }
