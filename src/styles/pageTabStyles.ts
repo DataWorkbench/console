d19fb51abf027e9a3f65e5c2191b708ca817e802
yazhouhu <yazhouhu@yunify.com>
2021-12-14 17:33:50 +0800

    style: table tab style

--- src/styles/pageTabStyles.ts
+++ src/styles/pageTabStyles.ts
@@ -1,11 +1,21 @@
 import tw, { css } from 'twin.macro'
 
 const pageTabStyles = css`
-  .page-tab-container .collapse-panel .tab-title {
-    ${tw`font-medium`}
-  }
-  .collapse-panel .panel-right .icon {
-    ${tw`-top-12`}
+  .page-tab-container {
+    .collapse-panel {
+      .tab-title {
+        ${tw`font-medium`}
+      }
+      .panel-right .icon {
+        ${tw`-top-12`}
+      }
+      .panel-left {
+        ${tw`py-6`}
+        .button-list {
+          ${tw`h-5`}
+        }
+      }
+    }
   }
 `
 
