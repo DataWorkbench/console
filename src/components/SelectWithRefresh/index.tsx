83d9b8f3e8912f3522722b32c44ffce69dfeb11b
yazhouhu <yazhouhu@yunify.com>
2021-12-20 16:44:53 +0800

    feat: fixed SelectWithRefresh ref warning

--- src/components/SelectWithRefresh/index.tsx
+++ src/components/SelectWithRefresh/index.tsx
@@ -2,7 +2,7 @@ import { css, styled } from 'twin.macro'
 import { Select, Button, Form } from '@QCFE/lego-ui'
 
 import { Icon } from '@QCFE/qingcloud-portal-ui'
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState, forwardRef, useImperativeHandle } from 'react'
 
 const SelectWithRefreshBox = styled('div')(() => [
   css`
@@ -15,25 +15,28 @@ interface ISelectWithRefreshProps {
   onRefresh: () => void
   [propName: string]: any
 }
-const SelectWithRefreshCmp = (props: ISelectWithRefreshProps) => {
-  const { onRefresh, onChange, help, labelClassName, label, ...rest } = props
-  const [domId] = useState(Math.random().toString(32))
-  return (
-    <SelectWithRefreshBox>
-      <>
-        <Select {...rest} id={domId} onChange={onChange} />
-        <Button tw="w-8 ml-3 p-0">
-          <Icon
-            name="refresh"
-            tw="text-xl  text-white"
-            size={20}
-            onClick={() => onRefresh && onRefresh()}
-          />
-        </Button>
-      </>
-    </SelectWithRefreshBox>
-  )
-}
+const SelectWithRefreshCmp = forwardRef(
+  (props: ISelectWithRefreshProps, ref: any) => {
+    const { onRefresh, onChange, help, labelClassName, label, ...rest } = props
+    const [domId] = useState(Math.random().toString(32))
+    useImperativeHandle(ref, () => ({}))
+    return (
+      <SelectWithRefreshBox>
+        <>
+          <Select {...rest} id={domId} onChange={onChange} />
+          <Button tw="w-8 ml-3 p-0">
+            <Icon
+              name="refresh"
+              tw="text-xl  text-white"
+              size={20}
+              onClick={() => onRefresh && onRefresh()}
+            />
+          </Button>
+        </>
+      </SelectWithRefreshBox>
+    )
+  }
+)
 
 export const SelectWithRefresh: (p: ISelectWithRefreshProps) => ReactElement = (
   Form as any
