113eb4db0c511f0d82fc96d291314e7e45fbfba1
yazhouhu <yazhouhu@yunify.com>
2021-12-20 17:17:03 +0800

    fix: fixed SelectWithRefresh ref warnning

--- src/components/SelectWithRefresh/index.tsx
+++ src/components/SelectWithRefresh/index.tsx
@@ -2,7 +2,13 @@ import { css, styled } from 'twin.macro'
 import { Select, Button, Form } from '@QCFE/lego-ui'
 
 import { Icon } from '@QCFE/qingcloud-portal-ui'
-import { ReactElement, useState, forwardRef, useImperativeHandle } from 'react'
+import {
+  ReactElement,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react'
 
 const SelectWithRefreshBox = styled('div')(() => [
   css`
@@ -15,28 +21,32 @@ interface ISelectWithRefreshProps {
   onRefresh: () => void
   [propName: string]: any
 }
-const SelectWithRefreshCmp = forwardRef(
-  (props: ISelectWithRefreshProps, ref: any) => {
-    const { onRefresh, onChange, help, labelClassName, label, ...rest } = props
-    const [domId] = useState(Math.random().toString(32))
-    useImperativeHandle(ref, () => ({}))
-    return (
-      <SelectWithRefreshBox>
-        <>
-          <Select {...rest} id={domId} onChange={onChange} />
-          <Button tw="w-8 ml-3 p-0">
-            <Icon
-              name="refresh"
-              tw="text-xl  text-white"
-              size={20}
-              onClick={() => onRefresh && onRefresh()}
-            />
-          </Button>
-        </>
-      </SelectWithRefreshBox>
-    )
-  }
-)
+const SelectWithRefreshCmp = forwardRef<
+  { getControlRef: () => HTMLSelectElement },
+  ISelectWithRefreshProps
+>((props, ref) => {
+  const { onRefresh, onChange, help, labelClassName, label, ...rest } = props
+  const selectRef = useRef()
+  const [domId] = useState(Math.random().toString(32))
+  useImperativeHandle(ref, () => ({
+    getControlRef: () => selectRef?.current!,
+  }))
+  return (
+    <SelectWithRefreshBox>
+      <>
+        <Select {...rest} id={domId} onChange={onChange} ref={selectRef} />
+        <Button tw="w-8 ml-3 p-0">
+          <Icon
+            name="refresh"
+            tw="text-xl  text-white"
+            size={20}
+            onClick={() => onRefresh && onRefresh()}
+          />
+        </Button>
+      </>
+    </SelectWithRefreshBox>
+  )
+})
 
 export const SelectWithRefresh: (p: ISelectWithRefreshProps) => ReactElement = (
   Form as any
