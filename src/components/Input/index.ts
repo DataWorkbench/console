905ef52cf7691999bd1d07ba1c53eb7a358dfced
yazhouhu <yazhouhu@yunify.com>
2022-02-11 14:56:02 +0800

    feat: inputField onchange

--- src/components/Input/index.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react'
-import { Form } from '@QCFE/lego-ui'
-import { Input, IInputProps } from './Input'
-
-export * from './Input'
-
-export const InputField: (p: IInputProps) => ReactElement = (
-  Form as any
-).getFormField(Input)
