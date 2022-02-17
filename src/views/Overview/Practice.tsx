63125bcea4fdca37f5361f441b674fb1de77bdb5
wickyang <wickyang@yunify.com>
2022-02-17 19:12:14 +0800

    chore: update best practice link

--- src/views/Overview/Practice.tsx
+++ src/views/Overview/Practice.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Card, CardHeader, CardContent, IconCard } from 'components'
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  IconCard,
+  HelpCenterLink,
+} from 'components'
 
 interface Props {
   className?: string
@@ -11,18 +17,28 @@ const Practice: React.FC<Props> = ({ className }) => {
       <CardHeader title="最佳实践" />
       <CardContent>
         <div tw="flex justify-center space-x-2 2xl:space-x-5">
-          <IconCard
-            tw="flex-1"
-            icon="templet"
-            title="沧州银行大数据工作台+弹性存储最佳实践"
-            subtitle="通用云上弹性服务器配合大数据处理的最佳实践"
-          />
-          <IconCard
-            icon="templet"
-            tw="flex-1"
-            title="大数据工作台流批一体最佳实践"
-            subtitle="通过流批一体的方式，轻量化解决企业数据处理"
-          />
+          <HelpCenterLink
+            href="/best-practices/practice01/summary/"
+            color=""
+            tw="flex-1 no-underline font-normal text-left"
+          >
+            <IconCard
+              icon="templet"
+              title="实时同步 MySQL 数据到 Elasticsearch"
+              subtitle="本实践为您介绍如何将 MySQL 数据实时（upsert 或 append）同步到 Elasticsearch"
+            />
+          </HelpCenterLink>
+          <HelpCenterLink
+            href="/best-practices/practice02/summary/"
+            color=""
+            tw="flex-1 no-underline font-normal text-left"
+          >
+            <IconCard
+              icon="templet"
+              title="实时计算 uv、pv、转化率（SQL 作业）"
+              subtitle="本小节为您介绍如何通过 SQL 作业对接网站的点击流数据，并对点击流数据进行实时分析，计算出独立访客数（uv）、页面访问数（pv）、转化率等指标"
+            />
+          </HelpCenterLink>
         </div>
       </CardContent>
     </Card>
