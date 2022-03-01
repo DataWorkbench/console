7745bb4f6fd3fc5f8f3c6300a098f2b8ba415f43
yazhouhu <yazhouhu@yunify.com>
2022-03-01 19:32:15 +0800

    feat: add member api

---
+++ src/stores/api/role.ts
@@ -0,0 +1,13 @@
+import { request } from 'utils'
+
+export interface IRoleParams {
+  regionId?: string
+  spaceId?: string
+  [k: string]: unknown
+}
+
+export const loadRoleList = ({ regionId, spaceId }: IRoleParams) =>
+  request({
+    region: regionId,
+    uri: `/v1/workspace/${spaceId}/role/system`,
+  })
