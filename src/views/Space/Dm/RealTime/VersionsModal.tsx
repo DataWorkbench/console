7c227e079b1444216f441d10a93c5a875387974a
caron.zhang <zhangxing@yunify.com>
2022-03-09 14:36:56 +0800

    feat: add sorter and remove search

--- src/views/Space/Dm/RealTime/VersionsModal.tsx
+++ src/views/Space/Dm/RealTime/VersionsModal.tsx
@@ -11,6 +11,7 @@ import { useStore } from 'stores'
 interface IFilter {
   limit: number
   offset: number
+  reverse: boolean
   search?: string
   sort_by?: string
 }
@@ -22,6 +23,8 @@ const JobVersions = (props: any) => {
   const [filter, setFilter] = useImmer<IFilter>({
     limit: 10,
     offset: 0,
+    sort_by: 'updated',
+    reverse: true,
   })
 
   const handleCheckJobByVersion = useCallback(
@@ -49,6 +52,10 @@ const JobVersions = (props: any) => {
       {
         title: '提交时间',
         dataIndex: 'updated',
+        sortable: true,
+        sortOrder:
+          // eslint-disable-next-line no-nested-ternary
+          filter.sort_by === 'updated' ? (filter.reverse ? 'asc' : 'desc') : '',
         render: (value: number) => (
           <div tw="text-neut-8">
             {dayjs(value * 1000).format('YYYY-MM-DD HH:mm:ss')}
@@ -69,7 +76,7 @@ const JobVersions = (props: any) => {
         ),
       },
     ],
-    [handleCheckJobByVersion]
+    [filter.reverse, filter.sort_by, handleCheckJobByVersion]
   )
 
   const { isFetching, data } = useQueryReleaseJobVersions(
@@ -90,21 +97,29 @@ const JobVersions = (props: any) => {
           </Button>
         }
       >
-        <div tw="px-5">
-          <InputSearch
-            tw="w-[328px] mt-6 mb-3"
-            css={css`
-              input {
-                ${tw`rounded-2xl`}
-              }
-            `}
-            placeholder="搜索操作人、版本、备注"
-          />
+        <div tw="px-5 pt-6">
+          {false && (
+            <InputSearch
+              tw="w-[328px] mt-6 mb-3"
+              css={css`
+                input {
+                  ${tw`rounded-2xl`}
+                }
+              `}
+              placeholder="搜索操作人、版本、备注"
+            />
+          )}
           <Table
             rowKey="version"
             columns={columns}
             loading={isFetching}
             dataSource={data?.infos || []}
+            onSort={(sortKey: any, order: string) => {
+              setFilter((draft) => {
+                draft.sort_by = sortKey
+                draft.reverse = order === 'asc'
+              })
+            }}
             pagination={{
               total: data?.total || 0,
               current: filter.offset / filter.limit + 1,
