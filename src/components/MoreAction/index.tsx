2c37a2c85c18aeb996041313f6f5a05ffa8c2bfb
yazhou <yazhouhu@yunify.com>
2022-04-08 11:10:40 +0800

    feat: add moreAction component

---
+++ src/components/MoreAction/index.tsx
@@ -0,0 +1,84 @@
+import { Icon } from '@QCFE/qingcloud-portal-ui'
+import { Menu } from '@QCFE/lego-ui'
+import React, { SyntheticEvent } from 'react'
+import tw, { css } from 'twin.macro'
+import { Center } from '../Center'
+import { Tooltip } from '../Tooltip'
+
+export interface IMoreActionItem {
+  key: string
+  text: string
+  icon?: string
+  value?: any
+}
+export interface IMoreActionProps {
+  theme?: 'darker' | 'light'
+  onMenuClick?: (selectedData: any, menuKey: string) => void
+  items: IMoreActionItem[]
+}
+
+const { MenuItem } = Menu as any
+
+export const moreActionStyle = {
+  child: ({ theme = 'darker' }: { theme: 'darker' | 'light' }) => {
+    return [
+      theme === 'darker' &&
+        css`
+          &[aria-expanded='true'] {
+            ${tw`bg-line-dark`}
+          }
+
+          svg {
+            ${tw`text-white! bg-transparent! fill-[transparent]!`}
+          }
+        `,
+    ]
+  },
+}
+
+export const MoreAction = (props: IMoreActionProps) => {
+  const { theme = 'darker', onMenuClick, items } = props
+
+  const handleMenuClick = (
+    e: SyntheticEvent,
+    key: string,
+    value: string | number
+  ) => {
+    // e.stopPropagation()
+    if (onMenuClick) {
+      onMenuClick(value, key)
+    }
+  }
+
+  return (
+    <Center>
+      <Tooltip
+        arrow={false}
+        trigger="click"
+        placement="bottom-end"
+        twChild={moreActionStyle.child({ theme }) as any}
+        content={
+          <Menu onClick={handleMenuClick}>
+            {items.map(({ key, value, text }) => (
+              <MenuItem key={key} value={value}>
+                {text}
+              </MenuItem>
+            ))}
+          </Menu>
+        }
+      >
+        <div tw="flex items-center p-0.5 cursor-pointer dark:hover:bg-line-dark rounded-sm">
+          <Icon
+            name="more"
+            clickable
+            changeable
+            type={theme !== 'light' ? 'light' : 'dark'}
+            size={20}
+          />
+        </div>
+      </Tooltip>
+    </Center>
+  )
+}
+
+// export default MoreAction
