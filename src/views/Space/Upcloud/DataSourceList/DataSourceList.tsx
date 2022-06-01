c1ff519ade2f977e4509aa3ea7fa982be423f22f
Dylan <253423846@qq.com>
2022-06-01 16:00:26 +0800

    fix: 表单操作文字颜色

--- src/views/Space/Upcloud/DataSourceList/DataSourceList.tsx
+++ src/views/Space/Upcloud/DataSourceList/DataSourceList.tsx
@@ -408,7 +408,7 @@ const DataSourceList = observer((props: DataSourceListProps) => {
         if (selectMode) {
           return (
             <span
-              tw="cursor-pointer"
+              tw="cursor-pointer text-blue-10 hover:text-blue-12"
               onClick={() => {
                 handlePing(info)
               }}
