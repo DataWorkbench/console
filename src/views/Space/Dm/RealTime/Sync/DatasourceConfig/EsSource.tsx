ff632dff4eafb346281a27a0d90fb3492416a407
亚洲 <git@yazhou.io>
2022-07-05 16:56:44 +0800

    feat: es source, es target

--- src/views/Space/Dm/RealTime/Sync/DatasourceConfig/EsSource.tsx
+++ src/views/Space/Dm/RealTime/Sync/DatasourceConfig/EsSource.tsx
@@ -11,10 +11,11 @@ import { useImmer } from 'use-immer'
 import { Form } from '@QCFE/qingcloud-portal-ui'
 import { map } from 'rxjs'
 import { get } from 'lodash-es'
-import { AffixLabel, Center, FlexBox } from 'components'
+import { AffixLabel, Center, FlexBox, HelpCenterLink } from 'components'
 import { Icon } from '@QCFE/lego-ui'
 import { IDataSourceConfigProps, ISourceRef } from './interfaces'
 import { source$ } from '../common/subjects'
+import { useDescribeDataSource } from '../../../../../../hooks'
 
 type FieldKeys = 'id' | 'index' | 'type' | 'batchSize'
 
@@ -34,6 +35,9 @@ const EsSource = forwardRef<ISourceRef, IDataSourceConfigProps>(
             }
             return {
               id: get(e, 'data.id'),
+              batchSize: get(e, 'data.batch_size', 1),
+              type: get(e, 'data.type'),
+              index: get(e, 'data.index'),
             }
           })
         )
@@ -54,11 +58,16 @@ const EsSource = forwardRef<ISourceRef, IDataSourceConfigProps>(
         getData: () => {
           return {
             id: dbInfo?.id,
+            batchSize: dbInfo?.batchSize,
+            type: dbInfo?.type,
+            index: dbInfo?.index,
           }
         },
         refetchColumn: () => {},
       }
     })
+
+    const { data: sourceDetail } = useDescribeDataSource(dbInfo.id)
 
     return (
       <Form css={styles.form} ref={sourceForm}>
@@ -71,32 +80,52 @@ const EsSource = forwardRef<ISourceRef, IDataSourceConfigProps>(
               value={dbInfo?.index}
               onChange={(e) => {
                 setDbInfo((draft) => {
-                  draft.index = e.target.value
+                  draft.index = e
                 })
               }}
+              validateOnChange
+              schemas={[
+                {
+                  rule: { required: true },
+                  help: (
+                    <div>
+                      <span>不能为空, </span>
+                      <span tw="text-font-placeholder mr-1">详见</span>
+                      <HelpCenterLink hasIcon isIframe={false} href="###">
+                        ElasticSearch Source 配置文档
+                      </HelpCenterLink>
+                    </div>
+                  ),
+                  status: 'error',
+                },
+              ]}
+              help={
+                <HelpCenterLink hasIcon isIframe={false} href="###">
+                  ElasticSearch Source 配置文档
+                </HelpCenterLink>
+              }
             />
-            <TextField
-              label={<AffixLabel required>类型</AffixLabel>}
-              name="type"
-              value={dbInfo?.type}
-              onChange={(e) => {
-                setDbInfo((draft) => {
-                  draft.type = e.target.value
-                })
-              }}
-              placeholder="ElasticSearch 中的 Index 名"
-            />
-            <TextField
-              label={<AffixLabel required>类型</AffixLabel>}
-              name="type"
-              value={dbInfo?.type}
-              onChange={(e) => {
-                setDbInfo((draft) => {
-                  draft.type = e.target.value
-                })
-              }}
-              placeholder="ElasticSearch 中的 Index 名"
-            />
+            {get(sourceDetail, 'url.elastic_search.version') === '6' && (
+              <TextField
+                label={<AffixLabel required>类型</AffixLabel>}
+                name="type"
+                value={dbInfo?.type}
+                onChange={(e) => {
+                  setDbInfo((draft) => {
+                    draft.type = e
+                  })
+                }}
+                placeholder="ElasticSearch 中的 Index 名"
+                validateOnChange
+                schemas={[
+                  {
+                    rule: { required: true },
+                    help: '请输入索引类型',
+                    status: 'error',
+                  },
+                ]}
+              />
+            )}
             <FlexBox>
               <div css={styles.line} />
               <Center
@@ -117,12 +146,25 @@ const EsSource = forwardRef<ISourceRef, IDataSourceConfigProps>(
                 label={<AffixLabel required>批量大小</AffixLabel>}
                 name="batchSize"
                 value={dbInfo?.batchSize}
-                onChange={(e) => {
+                onChange={(v) => {
                   setDbInfo((draft) => {
-                    draft.batchSize = e
+                    draft.batchSize = v
                   })
                 }}
+                min={1}
+                max={65535}
+                step={1}
+                help="1-65535"
                 placeholder="批量大小"
+                validateOnChange
+                schemas={[
+                  {
+                    rule: { required: true },
+                    help: '请设置批量大小',
+                    status: 'error',
+                  },
+                ]}
+                showButton={false}
               />
             )}
           </>
--- src/views/Space/Dm/RealTime/Sync/DatasourceConfig/EsTarget.tsx
+++ src/views/Space/Dm/RealTime/Sync/DatasourceConfig/EsTarget.tsx
@@ -11,10 +11,11 @@ import { useImmer } from 'use-immer'
 import { Form } from '@QCFE/qingcloud-portal-ui'
 import { map } from 'rxjs'
 import { get } from 'lodash-es'
-import { AffixLabel, Center, FlexBox } from 'components'
+import { AffixLabel, Center, FlexBox, HelpCenterLink } from 'components'
 import { Icon } from '@QCFE/lego-ui'
+import { useDescribeDataSource } from 'hooks'
 import { IDataSourceConfigProps, ISourceRef } from './interfaces'
-import { source$ } from '../common/subjects'
+import { target$ } from '../common/subjects'
 
 type FieldKeys = 'id' | 'index' | 'type' | 'batchSize' | 'keyDelimiter'
 
@@ -26,7 +27,7 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
     const [showAdvanced, setShowAdvanced] = useState(false)
 
     useLayoutEffect(() => {
-      const sub = source$
+      const sub = target$
         .pipe(
           map((e) => {
             if (!e) {
@@ -34,6 +35,10 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
             }
             return {
               id: get(e, 'data.id'),
+              batchSize: get(e, 'data.batch_size', 1),
+              keyDelimiter: get(e, 'data.key_delimiter'),
+              type: get(e, 'data.type'),
+              index: get(e, 'data.index'),
             }
           })
         )
@@ -43,6 +48,7 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
       }
     }, [setDbInfo])
 
+    const { data: sourceDetail } = useDescribeDataSource(dbInfo.id)
     useImperativeHandle(ref, () => {
       return {
         validate: () => {
@@ -54,6 +60,10 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
         getData: () => {
           return {
             id: dbInfo?.id,
+            batch_size: dbInfo?.batchSize,
+            key_delimiter: dbInfo?.keyDelimiter,
+            type: dbInfo?.type,
+            index: dbInfo?.index,
           }
         },
         refetchColumn: () => {},
@@ -62,54 +72,83 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
 
     return (
       <Form css={styles.form} ref={sourceForm}>
-        <BaseConfigCommon from="source" />
+        <BaseConfigCommon from="target" />
         {dbInfo?.id && (
           <>
             <TextField
               label={<AffixLabel required>索引</AffixLabel>}
               name="index"
               value={dbInfo?.index}
-              onChange={(e) => {
+              onChange={(v) => {
                 setDbInfo((draft) => {
-                  draft.index = e.target.value
+                  draft.index = v
                 })
               }}
+              validateOnChange
+              schemas={[
+                {
+                  rule: { required: true },
+                  help: (
+                    <div>
+                      <span>不能为空, </span>
+                      <span tw="text-font-placeholder mr-1">详见</span>
+                      <HelpCenterLink hasIcon isIframe={false} href="###">
+                        ElasticSearch Sink 配置文档
+                      </HelpCenterLink>
+                    </div>
+                  ),
+                  status: 'error',
+                },
+              ]}
+              help={
+                <HelpCenterLink hasIcon isIframe={false} href="###">
+                  ElasticSearch Sink 配置文档
+                </HelpCenterLink>
+              }
+              placeholder="请输入索引"
             />
-            <TextField
-              label={<AffixLabel required>类型</AffixLabel>}
-              name="type"
-              value={dbInfo?.type}
-              onChange={(e) => {
-                setDbInfo((draft) => {
-                  draft.type = e.target.value
-                })
-              }}
-              placeholder="ElasticSearch 中的 Index 名"
-            />
-            <TextField
-              label={<AffixLabel required>类型</AffixLabel>}
-              name="type"
-              value={dbInfo?.type}
-              onChange={(e) => {
-                setDbInfo((draft) => {
-                  draft.type = e.target.value
-                })
-              }}
-              placeholder="ElasticSearch 中的 Index 名"
-            />
+            {get(sourceDetail, 'url.elastic_search.version') === '6' && (
+              <TextField
+                label={<AffixLabel required>类型</AffixLabel>}
+                name="type"
+                value={dbInfo?.type}
+                onChange={(v) => {
+                  setDbInfo((draft) => {
+                    draft.type = v
+                  })
+                }}
+                validateOnChange
+                schemas={[
+                  {
+                    rule: { required: true },
+                    help: '请输入类型',
+                    status: 'error',
+                  },
+                ]}
+                placeholder="ElasticSearch 中的 Index 名"
+              />
+            )}
             <TextField
               label={<AffixLabel required>分隔符号</AffixLabel>}
               name="keyDelimiter"
               value={dbInfo?.keyDelimiter}
-              onChange={(e) => {
+              onChange={(v) => {
                 setDbInfo((draft) => {
-                  draft.keyDelimiter = e.target.value
+                  draft.keyDelimiter = v
                 })
               }}
               placeholder={
                 /* eslint-disable-next-line no-template-curly-in-string */
                 '文档 ID 之间的分隔符号，eg:“${col1}_${col2}” 默认值是下划线。'
               }
+              validateOnChange
+              schemas={[
+                {
+                  rule: { required: true },
+                  help: '请输入分隔符号',
+                  status: 'error',
+                },
+              ]}
             />
             <FlexBox>
               <div css={styles.line} />
@@ -131,12 +170,25 @@ const EsTarget = forwardRef<ISourceRef, IDataSourceConfigProps>(
                 label={<AffixLabel required>批量大小</AffixLabel>}
                 name="batchSize"
                 value={dbInfo?.batchSize}
-                onChange={(e) => {
+                onChange={(v) => {
                   setDbInfo((draft) => {
-                    draft.batchSize = e
+                    draft.batchSize = v
                   })
                 }}
+                min={1}
+                max={65535}
+                step={1}
+                help="1-65535"
                 placeholder="批量大小"
+                validateOnChange
+                schemas={[
+                  {
+                    rule: { required: true },
+                    help: '请设置批量大小',
+                    status: 'error',
+                  },
+                ]}
+                showButton={false}
               />
             )}
           </>
