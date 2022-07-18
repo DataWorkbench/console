11f84a3db9114457a6cd2bca4aa883405b9c27c8
亚洲 <git@yazhou.io>
2022-07-18 16:41:32 +0800

    feat: add auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { render } from 'react-dom'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import App from './App'
+import Auth from 'views/Auth/Auth'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import '@QCFE/qingcloud-portal-ui/lib/scss/qingcloud-portal-ui.min.css'
 import GlobalStyles from './styles/GlobalStyle'
 import 'tippy.js/dist/tippy.css'
 import 'simplebar-react/dist/simplebar.min.css'
 import './themes/colors/index.tpl'
+import App from './App'
 // import 'tippy.js/dist/svg-arrow.css'
 // import 'tippy.js/animations/scale-subtle.css'
 import 'dayjs/locale/zh-cn'
@@ -16,10 +18,22 @@ dayjs.extend(relativeTime)
 
 dayjs.locale('zh-cn')
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 render(
   <>
     <GlobalStyles />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Auth>
+        <App />
+      </Auth>
+    </QueryClientProvider>
   </>,
   document.getElementById('root')
 )
